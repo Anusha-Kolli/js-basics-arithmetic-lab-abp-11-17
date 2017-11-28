eab265628f421267148bdd1549452e98f07c26a1
Naga Anusha Kolli <kolli.anusha9@gmail.com>
2017-11-28 23:33:27 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -11,4 +11,4 @@ newID = 1000000000 + oldID;
 ageIsValid = Number.isInteger(currentAge);
 randomNumber = Math.random(0,20);
 randomInteger = Math.floor(randomNumber);
-randomUserID = randomInteger;
+randomUserID = Number.isInteger(randomUserID);
